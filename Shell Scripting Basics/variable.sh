765563b00c74e3cd62e3c8e93ead35d8ad79635c
Neha Thorat <thoratneha18@gmail.com>
2026-05-03 23:13:18 +0530

    Add variable script to greet user by name and role

---
+++ Shell Scripting Basics/variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This is variables.sh file
+
+read -p "Enter your name: " NAME
+read -p "Enter your role: " ROLE
+
+echo "Hello, I'm $NAME and I am a $ROLE"
+echo 'Hello, Im $NAME and I am a $ROLE'
